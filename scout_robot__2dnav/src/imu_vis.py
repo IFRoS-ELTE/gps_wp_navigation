9ac08085a33a6d5611607a149bae388389f69286
Zewdie Habtie <zewdiehabtie26@gmail.com>
2024-11-27 12:40:26 +0100

    cmd_vel not working

--- scout_robot__2dnav/src/imu_vis.py
+++ scout_robot__2dnav/src/imu_vis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import rospy
+import math
 from sensor_msgs.msg import Imu
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point,Quaternion,Vector3Stamped, Twist
@@ -7,7 +8,6 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import atan2, degrees
 import os
 import numpy as np
-import csv
 class IMUOrientationVisualizer:
     def __init__(self):
         rospy.init_node("mag_calibration", anonymous=True)
@@ -15,60 +15,72 @@ class IMUOrientationVisualizer:
         rospy.Subscriber('/imu/data', Imu, self.imu_callback)
         rospy.Subscriber('/imu/mag', Vector3Stamped, self.imu_mag_callback)
 
-        self.imu_pub = rospy.Publisher("imu_orientn", Marker, queue_size=10)
-        self.imu_mag_pub = rospy.Publisher("imu_mag", Marker, queue_size=10)
+        self.mag_values = []
 
+        #headings vars
+        self.heading_tn = None
+        self.rate = rospy.Rate(10)
+ 
         self.mag_heading = None
-
+ 
         # mag declination
-        self.declination = None #! for now..change later
+        self.declination = 5.783333333
+ 
+        self.seq_num = 0
 
-        self.output_file = rospy.get_param("~output_file", "magnetometer_data.csv")
-        # Open file for writing
-        self.file = open(self.output_file, 'w', newline='')
-        self.csv_writer = csv.writer(self.file)
+        self.imu_pub = rospy.Publisher("imu_orntn", Marker, queue_size=10)
+        self.imu_mag_pub = rospy.Publisher("mag_orntn", Marker, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
-        # Write header
-        self.csv_writer.writerow(['x', 'y', 'z'])
+        self.mag_values_acc = np.array([0, 0, 0]) #accumulate 15 imu/mag values
 
-        self.mag_points = np.empty((0, 3), float)
     def imu_mag_callback(self, msg):
 
-        ###? Personal observation: 
-        #* The angle computed usign atan2 from the mag vector assumes clockwise rotation is positive
-        #* However, the quaternion from the imu assumes counter-clockwise rotation is positive
-        x = msg.vector.x
-        y = msg.vector.y
-        z = msg.vector.z
-
-        self.mag_points = np.vstack((self.mag_points, [x, y, z]))
-        self.csv_writer.writerow([x, y, z])
-        #######################!After calibration####################################
-        #* Matrix A for soft iron calibration
-        A = np.empty((3, 3), float)
-
-        #* vector b for soft iron calibration
-        b = np.empty((1, 3), float)
-
-        C = (np.array([x, y, z].reshape(-1, 1) - b)) @ A
-        x = C[0], y = C[1], z = C[2]
-        ###########################!###############################################
-        heading = atan2(y, x) #assuming heading is in x axis
-        heading_calib = -atan2(y, x) # to match the quaternion orientation
-
-        #* Low-pass filter the heading
-        if self.mag_heading is None:
-             self.mag_heading = heading
-        else:
-             self.mag_heading = 0.9 * self.mag_heading + 0.1 * heading
-
-        heading_deg = degrees(self.mag_heading)  + self.declination
-        heading_deg_calib = degrees(heading_calib)  + self.declination
+        # Hard iron effect correction
+        x = msg.vector.x + 0.1908 
+        y = msg.vector.y + 0.0518
+        z = msg.vector.z + 0.5559
         
-        print(f"lpass heading: {heading_deg:.2f}| calib heading: {heading_deg_calib:.2f}")
+        self.mag_values.append([x, y, z])
 
+        heading_mag = -atan2(y, x) #rad
+        heading_tru = heading_mag + np.deg2rad(self.declination) #rad
+
+        #Normalize heading
+        heading_tru = (heading_tru + 2 * math.pi) % (2 * math.pi) - math.pi/2
+        self.heading_tn = heading_tru
+        print("TN_HEADING: ", degrees(self.heading_tn))
+        self.visualize_mag_heading()
+
+    def rotate_to_east(self):
+        if self.heading_tn is None:
+            print("NO HEADING DATA")
+            return
+        
+        heading_east = -math.pi / 2
+        delta_heading = heading_east - self.heading_tn
+
+        # Normalize d_heading
+        delta_heading = (delta_heading + math.pi) % (2 * math.pi) - math.pi
+
+        twist   = Twist()
+        w       = 0.5 #rad/s
+
+        if abs(delta_heading) > 0.05:
+            twist.angular.z = w if delta_heading > 0 else -w 
+            self.cmd_vel_pub.publish(twist)
+        else:
+            twist.angular.z = 0
+            self.cmd_vel_pub.publish(twist) 
+            print("ROBOT IS NOW FACING EAST!!! \nCLOSE THIS NODE")  
+
+    #save imu/mag to text file
+    def save_mag_value(self):
+         np.savetxt(rospy.Time.now(), np.array(self.mag_values))
+
+    def visualize_mag_heading(self):
         marker = Marker()
-        marker.header.frame_id = "base_link" #! to see it move with the robot
+        marker.header.frame_id = "odom" 
         marker.header.stamp = rospy.Time.now()
         marker.ns = "imu_mag_orientation"
         marker.id = 1
@@ -77,7 +89,7 @@ class IMUOrientationVisualizer:
         marker.action = Marker.ADD
 
         # Convert heading (yaw) to quaternion
-        quaternion = quaternion_from_euler(0, 0, -self.mag_heading)
+        quaternion = quaternion_from_euler(0, 0, self.heading_tn)
         marker.pose.orientation = Quaternion(*quaternion)
 
         marker.scale.x = 0.1
@@ -97,11 +109,12 @@ class IMUOrientationVisualizer:
         self.imu_mag_pub.publish(marker)
 
     def imu_callback(self, msg):
-        os.system('clear' if os.name == 'posix' else 'cls')
+        # os.system('clear' if os.name == 'posix' else 'cls')
 
         quaternion = msg.orientation
         roll, pitch, yaw = euler_from_quaternion([quaternion.x, quaternion.y, quaternion.z, quaternion.w])
-        print(f"Yaw_m: {yaw:.2f}({yaw*180/3.1415:.2f})")
+        # print(f"Yaw_IMU: {yaw:.2f}({yaw*180/3.1415:.2f})")
+        print("----------------------------")
         #create marker msg
         marker = Marker()
         marker.header.frame_id = "odom" #! to see it move with the robot
@@ -132,7 +145,10 @@ class IMUOrientationVisualizer:
         self.imu_pub.publish(marker)
 
     def run(self):
-            rospy.spin()
+        while not rospy.is_shutdown():
+            self.rotate_to_east()
+            self.rate.sleep()
+            # rospy.spin()
 if __name__ == '__main__':
     try:
         visualizer = IMUOrientationVisualizer()
